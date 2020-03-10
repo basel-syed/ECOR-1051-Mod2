b0b4050e24167be5c770b93cacfb9afda6a39e0f
Basel Syed <13090285+basel-syed@users.noreply.github.com>
2020-03-10 14:53:02 -0400

    Update L2-16-red.py

--- Milestone1/L2-16-red.py
+++ Milestone1/L2-16-red.py
@@ -1,11 +1,7 @@
 from Cimpl import *
 
 def red_channel(image: Image) -> Image:
-    """Return an image that has only the blue channel of an original image.
-    Written by: Basel Syed
 
-    >>> red_channel(p2-original)
-    """
     red_channel_image = copy(image)
     for x, y, (r, g, b) in red_channel_image:
         redfill = create_color(r, 0, 0)
@@ -15,12 +11,7 @@ def red_channel(image: Image) -> Image:
 
 
 def test_red(image: Image) -> bool:
-    """Returns true all pixels in image filtered by red_channel
-    Writtern by: Basel Syed
 
-    >>>test_red("p2-original.jpg")
-    True
-    """
     compared_image = red_channel(image)
     for x, y, (r, g, b) in compared_image:
         if not (g == 0 and b == 0):
