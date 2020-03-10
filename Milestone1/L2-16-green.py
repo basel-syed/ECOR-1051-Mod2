9e43bd6d6448f0b7918b346049306f1fc5213858
Basel Syed <13090285+basel-syed@users.noreply.github.com>
2020-03-10 14:52:35 -0400

    Update L2-16-green.py

--- Milestone1/L2-16-green.py
+++ Milestone1/L2-16-green.py
@@ -2,12 +2,7 @@ from Cimpl import *
 
 
 def greenChannel(image: Image) -> Image:
-    """Filters out the Blue and Red channels of an image, leaving the green chanel as is
-    Written by: Kevin Bai
-    >>> image = load_image(choose_file())         "miss_sullivan.jpg"
-    >>> turnedGreenLol = greenChannel(image)
-    >>> show(turnedGreenLol)
-    """
+
     new_image = copy(image)
 
     # Set the intensities of blue and red to 0 in every pixel.
@@ -19,14 +14,6 @@ def greenChannel(image: Image) -> Image:
 
 
 def testGreenChannel(testThisImage: Image) -> bool:
-    """Return whehter every pixel in image filtered by greenChannel has blue and red values equal to 0
-    Written by: Kevin Bai
-    >>>image = load_image(choose_file("miss_sullivan.jpg"))
-    >>> turnedGreenLol = greenChannel(image)
-    >>> show(turnedGreenLol)
-    >>>testGreenChannel(image)
-    True
-    """
     comparison = greenChannel(testThisImage)
 
     for x, y, (r, g, b) in comparison:
